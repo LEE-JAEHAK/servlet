59a1bb0f13cb1492bdc6b0374ebea09b8fde07cb
jerry <wogkr101@gmail.com>
2022-05-04 17:55:49 +0900

    5/4 JPA

---
+++ src/main/java/hello/servlet/service/posts/PostsService.java
@@ -0,0 +1,35 @@
+package hello.servlet.service.posts;
+
+
+import hello.servlet.domain.posts.Posts;
+import hello.servlet.domain.posts.PostsRepository;
+import hello.servlet.web.dto.PostsResponseDto;
+import hello.servlet.web.dto.PostsSaveRequestDto;
+import hello.servlet.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        return new PostsResponseDto(entity);
+    }
+}
