04f9d80534b4b99955f88e271b24aa88028d0ec9
jerry <wogkr101@gmail.com>
2022-05-20 17:55:44 +0900

    5/20 mustache ~148p

--- src/main/java/hello/servlet/service/posts/PostsService.java
+++ src/main/java/hello/servlet/service/posts/PostsService.java
@@ -3,6 +3,7 @@ package hello.servlet.service.posts;
 
 import hello.servlet.domain.posts.Posts;
 import hello.servlet.domain.posts.PostsRepository;
+import hello.servlet.web.dto.PostsListResponseDto;
 import hello.servlet.web.dto.PostsResponseDto;
 import hello.servlet.web.dto.PostsSaveRequestDto;
 import hello.servlet.web.dto.PostsUpdateRequestDto;
@@ -10,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,4 +35,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
 }
