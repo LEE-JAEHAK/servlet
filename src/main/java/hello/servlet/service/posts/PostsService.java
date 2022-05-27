03058aedb2347851084b749573b1ee731e315959
jerry <wogkr101@gmail.com>
2022-05-27 17:45:43 +0900

    5/27 Front & Oauth ~175p

--- src/main/java/hello/servlet/service/posts/PostsService.java
+++ src/main/java/hello/servlet/service/posts/PostsService.java
@@ -9,8 +9,8 @@ import hello.servlet.web.dto.PostsSaveRequestDto;
 import hello.servlet.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,6 +29,12 @@ public class PostsService {
         Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        postsRepository.delete(posts);
     }
 
     public PostsResponseDto findById(Long id){
